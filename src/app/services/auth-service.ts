76b59dd2405e9d0910771700ed151f99683f97aa
Abby <abigailcarey123@gmail.com>
2019-09-27 11:44:38 -0700

    Add logging and and out toggle method

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -6,31 +6,61 @@ import * as firebase from "firebase";
 })
 export class AuthService {
   db = firebase.firestore();
+  loggedIn = false;
+  userName = '';
+  
 
   constructor() { 
+    firebase.auth().onAuthStateChanged(this.authStateObserver);
   }
 
-    // Signs-in Friendly Chat.
-    signIn() {
-        // Sign into Firebase using popup auth & Google as the identity provider.
-        var provider = new firebase.auth.GoogleAuthProvider();
-        
-        firebase.auth().signInWithPopup(provider);
-        console.log(this.getUserName());
+    signAction() {
+      if (!this.loggedIn) {
+        this.signIn();
+        console.log('Signed in');
+        this.userName = this.getUserName();
+
+      } else {
+        this.signOut();
+        console.log('Signed out');
+        this.userName = '';
+      }
     }
 
-    // Signs-out of Friendly Chat.
+    // Signs-in pet app
+    signIn() {
+        // Sign into Firebase using popup auth & Google as the identity provider.
+        let provider = new firebase.auth.GoogleAuthProvider();
+        
+        firebase.auth().signInWithPopup(provider);
+        //console.log(this.getUserName());
+        this.loggedIn = true;
+    }
+
+    // Signs-out of pet app
     signOut() {
         // Sign out of Firebase.ß
         firebase.auth().signOut();
+        this.loggedIn = false;
+    }
+
+    // Initiate firebase auth.
+    initFirebaseAuth() {
+        // Listen to auth state changes.
+        firebase.auth().onAuthStateChanged(this.authStateObserver);
     }
 
     // Triggers when the auth state change for instance when the user signs-in or signs-out.
     authStateObserver(user) {
-      if (user) { // User is signed in!
-        console.log("A user!");
-      }
+    if (user) { // User is signed in!
+      // Get the signed-in user's profile pic and name.
+      //var profilePicUrl = this.getProfilePicUrl();
+      this.userName = firebase.auth().currentUser.displayName;
+      console.log(this.userName);
+    } else {
+        this.userName = '';
     }
+  }
 
     // Returns the signed-in user's profile pic URL.
     getProfilePicUrl() {
